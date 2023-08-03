7f678fb5df1b86619f11cb960d3d6ccfd1b1528d
BBrt78 <103853975+BBrt78@users.noreply.github.com>
2023-08-03 17:54:19 +0200

    Update Todo.js

--- Todo.js
+++ Todo.js
@@ -1,53 +1,47 @@
 import {useState, useRef, useEffect, useReducer, createContext} from 'react';
 import {TodoList} from "./Todolist.js";
 
-
 export function Todo(props) {
     const [edit, setEdit] = useState("Edit");
-    const [delete1, setDelete1] = useState(false);
+    const [editTitle, setEditTitle] = useState(props.title);
+    const [editContent, setEditContent] = useState(props.content);
    
     const editF = () => {
+        console.log(props.title)
         if (edit === "Edit") {
+
             setEdit("Save")
         } else {
             setEdit("Edit")}
     };
 
-    // const deleteTodo = () => {
-    //    delete1 === false ? setDelete1(true) : setDelete1(false);
-    // };
+    const editTitleF = (e) => {
+        setEditTitle(e.target.value);
+    }
+
+    const editContentF = (e) => {
+        setEditContent(e.target.value)
+    }
     
-    return (<>
-            {props.deleteClick === false ?
-                <>
+    return (    
                 <div className="todo">
                     {edit === "Edit" ?
                         <>
-                        <div className="title" id="titleId">Title: {props.title}</div><br />
-                        <div className="content2">Content: {props.content}</div><br />
+                        <div className="title" id="titleId">Title: {editTitle}</div><br />
+                        <div className="content2">Content: {editContent}</div><br />
                         <div>Added {props.date}</div>
                         </>
                     :
                         <>
                         <span className="titleEdit">Title: </span>
-                        <input type="text" id="title" value={props.title} ></input>
+                        <input type="text" id="title" value={editTitle} onChange={editTitleF}></input>
                         
                         <span>Content: </span>
-                        <textarea 
-                            className="contentEdit" 
-                            id="content"
-                             
-                            value={props.content}>
-                        </textarea>
+                        <input type="text" id="content" value={editContent} onChange={editContentF}></input>
                         </>
                     }
                     <button className="edit" onClick={editF}>{edit}</button>
                     <button className="delete" onClick={props.delete}>Delete</button>
-                </div>
-                </>
-            :
-            <></>
-            }
-        </>
+               </div>
     )
 }
